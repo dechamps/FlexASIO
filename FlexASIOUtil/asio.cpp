d42407fd39ea26ca3017c3086ba91fe943ccdaa5
Etienne Dechamps <etienne@edechamps.fr>
2018-10-24 22:26:31 +0100

    Fix corrupted ASIO timestamps and sample counts due to endianness.
    Shockingly, ASIO 64-bit structures put the most significant 32 bits
    (`hi`) *first* in the ASIOSamples and ASIOTimeStamp structures,
    even on little-endian architectures (such as x86). Unfortunately
    FlexASIO fell into that trap, resulting in corrupted timestamps that
    are completely unusable.
    
    This commit fixes the issue by swapping the 32-bit halves of the
    64-bit integer when running on a little endian architecture.
    
    Fixes #9, most likely.

--- FlexASIOUtil/asio.cpp
+++ FlexASIOUtil/asio.cpp
@@ -1,11 +1,33 @@
 #include "asio.h"
 
+#include <cassert>
 #include <cstring>
+#include <utility>
 
 #include "..\ASIOSDK2.3.1\common\asio.h"
 
 namespace flexasio {
+	namespace {
+		enum class Endianness { LITTLE, BIG };
+		Endianness GetEndianness() {
+			uint16_t uint16 = 1;
+			char bytes[sizeof uint16];
+			memcpy(bytes, &uint16, sizeof uint16);
+			if (bytes[0] == 1 && bytes[1] == 0) {
+				return Endianness::LITTLE;
+			}
+			if (bytes[0] == 0 && bytes[1] == 1) {
+				return Endianness::BIG;
+			}
+			abort();
+		}
+	}
+
+	// Somewhat surprisingly, ASIO 64-bit integer data types store the most significant 32 bits half *first*, even on little endian architectures. See the ASIO SDK documentation.
+	// On x86, which is little endian, that means we can't simply represent the value as an int64_t - we need to swap the two halves first.
+
 	template <typename ASIOInt64> int64_t ASIOToInt64(ASIOInt64 asioInt64) {
+		if (GetEndianness() == Endianness::LITTLE) std::swap(asioInt64.hi, asioInt64.lo);
 		int64_t result;
 		static_assert(sizeof asioInt64 == sizeof result);
 		memcpy(&result, &asioInt64, sizeof result);
@@ -18,6 +40,7 @@ namespace flexasio {
 		ASIOInt64 result;
 		static_assert(sizeof int64 == sizeof result);
 		memcpy(&result, &int64, sizeof result);
+		if (GetEndianness() == Endianness::LITTLE) std::swap(result.hi, result.lo);
 		return result;
 	}
 	template ASIOTimeStamp Int64ToASIO<ASIOTimeStamp>(int64_t);
