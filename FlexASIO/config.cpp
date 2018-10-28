80912974dc77cabdc33428a41f366a7c41eddee8
Etienne Dechamps <etienne@edechamps.fr>
2018-10-28 18:15:08 +0000

    Refactor and improve TOML option handling.
    This makes the code more concise and improves error messages.

--- FlexASIO/config.cpp
+++ FlexASIO/config.cpp
@@ -53,6 +53,64 @@ namespace flexasio {
 				return std::nullopt;
 			}
 		}
+
+		template <typename T> struct TomlValueTraits;
+		template <> struct TomlValueTraits<bool> {
+			constexpr static toml::Value::Type type = toml::Value::BOOL_TYPE;
+		};
+		template <> struct TomlValueTraits<int64_t> {
+			constexpr static toml::Value::Type type = toml::Value::INT_TYPE;
+		};
+		template <> struct TomlValueTraits<int> {
+			constexpr static toml::Value::Type type = toml::Value::INT_TYPE;
+		};
+		template <> struct TomlValueTraits<double> {
+			constexpr static toml::Value::Type type = toml::Value::DOUBLE_TYPE;
+		};
+		template <> struct TomlValueTraits<std::string> {
+			constexpr static toml::Value::Type type = toml::Value::STRING_TYPE;
+		};
+		template <> struct TomlValueTraits<toml::Time> {
+			constexpr static toml::Value::Type type = toml::Value::TIME_TYPE;
+		};
+		template <> struct TomlValueTraits<toml::Array> {
+			constexpr static toml::Value::Type type = toml::Value::ARRAY_TYPE;
+		};
+		template <> struct TomlValueTraits<toml::Table> {
+			constexpr static toml::Value::Type type = toml::Value::TABLE_TYPE;
+		};
+
+		constexpr const std::pair<toml::Value::Type, std::string_view> tomlTypes[] = {
+			{toml::Value::NULL_TYPE, "NULL"},
+			{toml::Value::BOOL_TYPE, "BOOL"},
+			{toml::Value::INT_TYPE, "INT"},
+			{toml::Value::DOUBLE_TYPE, "DOUBLE"},
+			{toml::Value::STRING_TYPE, "STRING"},
+			{toml::Value::TIME_TYPE, "TIME"},
+			{toml::Value::ARRAY_TYPE, "ARRAY"},
+			{toml::Value::TABLE_TYPE, "TABLE"},
+		};
+
+		template <typename T> auto GetValue(const toml::Value& table, const std::string& key) -> std::optional<T> {
+			const auto option = table.find(key);
+			if (option == nullptr) return std::nullopt;
+			if (!option->is<T>()) {
+				std::stringstream error;
+				error << "configuration option '" << key << "' is of type " << Find(option->type(), tomlTypes).value_or("(unknown)") << ", expected " << Find(TomlValueTraits<T>::type, tomlTypes).value_or("(unknown)");
+				throw std::runtime_error(error.str());
+			}
+			return option->as<T>();
+		}
+
+		template <typename T> struct RemoveOptional { using Value = T; };
+		template <typename T> struct RemoveOptional<std::optional<T>> { using Value = T; };
+
+		template <typename T> void SetOption(const toml::Value& table, const std::string& key, T& option) {
+			const auto value = GetValue<RemoveOptional<T>::Value>(table, key);
+			if (!value.has_value()) return;
+			option = *value;
+		}
+
 	}
 
 	std::optional<Config> LoadConfig() {
@@ -61,22 +119,13 @@ namespace flexasio {
 
 		Config config;
 
-		const auto backend = tomlValue->find("backend");
-		if (backend != nullptr) {
-			if (!backend->is<std::string>()) {
-				Log() << "Configuration error: backend value must be a string";
-				return std::nullopt;
-			}
-			config.backend = backend->as<std::string>();
+		try {
+			SetOption(*tomlValue, "backend", config.backend);
+			SetOption(*tomlValue, "wasapiExclusiveMode", config.wasapiExclusiveMode);
 		}
-
-		const auto wasapiExclusiveMode = tomlValue->find("wasapiExclusiveMode");
-		if (wasapiExclusiveMode != nullptr) {
-			if (!wasapiExclusiveMode->is<bool>()) {
-				Log() << "Configuration error: wasapiExclusiveMode value must be a bool";
-				return std::nullopt;
-			}
-			config.wasapiExclusiveMode = wasapiExclusiveMode->as<bool>();
+		catch (const std::exception& exception) {
+			Log() << "Invalid configuration: " << exception.what();
+			return std::nullopt;
 		}
 
 		return config;
