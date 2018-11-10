792218fa17c85e6180abb76e832d9b4afa987760
Etienne Dechamps <etienne@edechamps.fr>
2018-11-10 12:15:11 +0000

    Add a console application for listing PortAudio devices.
    See #22.

---
+++ PortAudioDevices/list.cpp
@@ -0,0 +1,70 @@
+#include "portaudio.h"
+
+#include <iostream>
+#include <string>
+
+
+namespace flexasio {
+	namespace {
+
+		template <typename Result>
+		auto ThrowOnPaError(Result result) {
+			if (result >= 0) return result;
+			throw std::runtime_error(std::string("PortAudio error ") + Pa_GetErrorText(result));
+		}
+
+		void PrintDevice(PaDeviceIndex deviceIndex) {
+			std::cout << "Device index: " << deviceIndex << std::endl;
+
+			const auto device = Pa_GetDeviceInfo(deviceIndex);
+			if (device == nullptr) throw std::runtime_error("Pa_GetDeviceInfo() returned NULL");
+
+			std::cout << "Device name: " << device->name << std::endl;
+
+			std::cout << std::endl;
+		}
+
+		void ListDevices() {
+			const PaDeviceIndex deviceCount = Pa_GetDeviceCount();
+
+			for (PaDeviceIndex deviceIndex = 0; deviceIndex < deviceCount; ++deviceIndex) {
+				try {
+					PrintDevice(deviceIndex);
+				}
+				catch (const std::exception& exception) {
+					std::cerr << "Error while printing device index " << deviceIndex << ": " << exception.what() << std::endl;
+				}
+			}
+		}
+
+		void InitAndListDevices() {
+			try {
+				ThrowOnPaError(Pa_Initialize());
+			}
+			catch (const std::exception& exception) {
+				throw std::runtime_error(std::string("failed to initialize PortAudio: ") + exception.what());
+			}
+
+			ListDevices();
+
+			try {
+				ThrowOnPaError(Pa_Terminate());
+			}
+			catch (const std::exception& exception) {
+				throw std::runtime_error(std::string("failed to terminate PortAudio: ") + exception.what());
+			}
+		}
+
+	}
+}
+
+int main(int, char**) {
+	try {
+		::flexasio::InitAndListDevices();
+	}
+	catch (const std::exception& exception) {
+		std::cerr << "ERROR: " << exception.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
