35cdfb8c654fa15c2bf3f539a5bfb0bd216ca4b0
Etienne Dechamps <etienne@edechamps.fr>
2019-01-12 10:31:18 +0000

    Make sure git describe is run against the most specific source dir.

--- src/installer.cmake
+++ src/installer.cmake
@@ -2,6 +2,7 @@ list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/dechamps_CMakeUtils")
 find_package(InnoSetup REQUIRED)
 
 find_package(Git REQUIRED)
+set(DECHAMPS_CMAKEUTILS_GIT_DIR "${CMAKE_CURRENT_LIST_DIR}/flexasio")
 include(version/version)
 string(REGEX REPLACE "^flexasio-" "" FLEXASIO_VERSION "${DECHAMPS_CMAKEUTILS_GIT_DESCRIPTION_DIRTY}")
 
