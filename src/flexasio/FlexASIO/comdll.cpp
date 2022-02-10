dbaefa204920a7d01caefcf461e82a726dfb365b
Etienne Dechamps <etienne@edechamps.fr>
2022-02-10 18:44:30 +0000

    Improve handling of external warnings

--- src/flexasio/FlexASIO/comdll.cpp
+++ src/flexasio/FlexASIO/comdll.cpp
@@ -1,7 +1,4 @@
-#pragma warning(push)
-#pragma warning(disable:28204 6001 6011 6385 6387)
 #include <atlbase.h>
-#pragma warning(pop)
 #include <atlcom.h>
 
 // This implements DLL entry points so that the automagic ATL module can do its thing. Nothing to see here, move along.
@@ -9,11 +6,8 @@
 class COMDLL : public CAtlDllModuleT<COMDLL> { };
 static COMDLL comdll;
 
-#pragma warning(push)
-#pragma warning(disable:28251 28252)
 extern "C" BOOL WINAPI DllMain(HINSTANCE, DWORD dwReason, LPVOID lpReserved) { return comdll.DllMain(dwReason, lpReserved); }
-STDAPI DllCanUnloadNow(void) { return comdll.DllCanUnloadNow(); }
-STDAPI DllGetClassObject(__in REFCLSID rclsid, __in REFIID riid, __deref_out LPVOID* ppv) { return comdll.DllGetClassObject(rclsid, riid, ppv); }
+__control_entrypoint(DllExport) STDAPI DllCanUnloadNow(void) { return comdll.DllCanUnloadNow(); }
+_Check_return_ STDAPI DllGetClassObject(__in REFCLSID rclsid, __in REFIID riid, __deref_out LPVOID* ppv) { return comdll.DllGetClassObject(rclsid, riid, ppv); }
 STDAPI DllRegisterServer(void) { return comdll.DllRegisterServer(); }
 STDAPI DllUnregisterServer(void) { return comdll.DllUnregisterServer(); }
-#pragma warning(pop)
